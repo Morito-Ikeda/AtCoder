6dd3ddc676494d918dd30f6dd16075dce037b7bf
Morito-Ikeda <ikeda.morito@gmail.com>
2019-02-03 15:30:09 +0900

    edpc b

--- EDPC/b.cpp
+++ EDPC/b.cpp
@@ -7,7 +7,7 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 const long long INF = 1LL << 60;
 
 // 入力
-int N;
+int N, K;
 long long h[100010];
 
 // DPテーブル
@@ -20,9 +20,10 @@ int main() {
   for (int i = 0; i < 100010; i++) dp[i] = INF;
   dp[0] = 0;
 
-  for (int i = 1; i < N; i++) {
-    chmin(dp[i], dp[i-1] + abs(h[i] - h[i-1]));
-    if (i > 1) chmin(dp[i], dp[i-2] + abs(h[i] - h[i-2]));
+  for (int i = 0; i < N; i++) {
+    for (int k = 1; k <= K; k++) {
+      chmin(dp[i+k], dp[i] + abs(h[i] - h[i+k]));
+    }
   }
 
   cout << dp[N-1] << endl;
